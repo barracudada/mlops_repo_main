8ef8af954a0434ddd06a88da5487c5f1dbcc48f4
barracudada <skirill720@yandex.ru>
2023-05-01 13:42:47 +0300

    Zero point

---
+++ main/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Git repo for MlOps Course for ODS',
+    author='Kirill',
+    license='',
+)
